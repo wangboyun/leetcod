c76d4dd58e913b9ed1b35c5201b1dc6bf25ec43b
wangboyun <501826086@qq.com>
2022-06-23 22:09:28 +0800

    add dynamic plan

--- 96.不同的二叉搜索树.cpp
+++ 96.不同的二叉搜索树.cpp
@@ -12,14 +12,21 @@
 #include <vector>
 using namespace std;
 
-
+#include <vector>
+using namespace std;
 // @lc code=start
 class Solution {
 public:
     
     int numTrees(int n) {
-        
-        return 
+        vector<int>dp(n + 1,0);
+        dp[0] = 1;
+        for(int i = 1; i <= n; ++i){
+            for(int j = 1; j <= i ; ++j){
+                dp[i] += dp[j-1] * dp[i - j];
+            }
+        }
+        return dp[n];
     }
 };
 // @lc code=end
